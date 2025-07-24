a7135c9e2994a6477523c38647d319d86b8c36d1
nandinidas07 <nandinidas107@gmail.com>
2025-07-24 20:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+const list = document.getElementById('infinite-list');
 
+let itemCount = 0;
+
+function addItems(count) {
+  for (let i = 0; i < count; i++) {
+    itemCount++;
+    const li = document.createElement('li');
+    li.innerText = Item ${itemCount};
+    list.appendChild(li);
+  }
+}
+
+
+addItems(10);
+
+list.addEventListener('scroll', function () {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2); 
+  }
+});
